2b7046ebdb282b680fdef2c06ea1e7e76ed1810f
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-10-27 17:37:50 -0500

    some change

--- Server/index.js
+++ Server/index.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const connectDB = require('./App/Config/db.js')
+const connectDB = require('./App/Config/db.js');
 const cors = require('cors');
-const authRoutes = require('./App/Routes/auth.js')
+const authRoutes = require('./App/Routes/auth.js');
 const issueRoutes = require('./App/Routes/Issue.js');
 const path = require('path');
+const fs = require('fs');
+
+
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+
 connectDB();
 
-app.get('/ping',(req,res)=>{
-    res.send('pong');
-})
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
 
 app.use(cors());
 app.use(express.json());
+
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/issues', issueRoutes);
 
-const port = process.env.PORT;
-app.listen(port,(req,res)=>{
-    console.log(`app is listening on port ${port}`);
-})
 
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}`);
+});
