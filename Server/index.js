1a588767770f7ebddb24145101032ecb2d27004a
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-10-27 14:08:01 -0500

    mongoose done

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,26 @@
-console.log("hello")
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const connectDB = require('./config/db')
+const cors = require('cors');
+const authRoutes = require('./routes/auth')
+const issueRoutes = require('./routes/issue');
+const path = require('path');
+connectDB();
+
+app.get('/ping',(req,res)=>{
+    res.send('pong');
+})
+
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/issues', issueRoutes);
+
+const port = process.env.PORT;
+app.listen(port,(req,res)=>{
+    console.log(`app is listening on port ${port}`);
+})
+
